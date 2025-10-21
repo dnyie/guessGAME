c21b58aa7d41d8481914ad5f7795d493503a8c4e
dan <dnyie.198@gmail.com>
2025-10-21 00:43:41 -0700

    final project

---
+++ pro.js
@@ -0,0 +1,48 @@
+"use strict";
+
+function restgame() {
+  rand = Math.trunc(Math.random() * 100);
+  document.querySelector(".guess").value = "";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = "5";
+  document.body.style.backgroundColor = "#222";
+  console.log("new number:", rand);
+}
+
+let rand = Math.trunc(Math.random() * 100);
+console.log("new number:", rand);
+let score = 5;
+
+document.querySelector(".again").addEventListener("click", restgame);
+
+document.querySelector(".check").addEventListener("click", function () {
+  const num = Number(document.querySelector(".guess").value);
+  if (score === 0) {
+    console.log("worst");
+    document.querySelector(".number").textContent = "😔😔";
+    setTimeout(restgame, 2000);
+  } else {
+    if (!num) {
+      document.querySelector(".number").textContent = "no NUM";
+      console.log("No Num");
+    } else if (num === rand) {
+      // console.log("nice");
+      document.querySelector(".number").textContent = "NICE😍😍";
+      document.querySelector(".message").textContent = "you are winnnn❤️";
+      console.log("Player is win");
+      setTimeout(restgame, 3000);
+    } else if (num > rand) {
+      score--;
+      document.querySelector(".message").textContent = "smaler⬇️⬆";
+      document.querySelector(".score").textContent = score;
+    } else if (num < rand) {
+      score--;
+      document.querySelector(".message").textContent = "biger⬆️";
+      document.querySelector(".score").textContent = score;
+    } else {
+      console.log("NOT TRUE");
+      console.log("Not True");
+    }
+  }
+});
